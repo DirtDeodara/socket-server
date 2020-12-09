4740dd32d9dab52eaf548a3e2801a0e2fb733ad7
Jenna Goldman <jennagoldman@lithia.com>
2020-12-08 19:55:50 -0800

    adjust get route

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 
-router.get('/', cors(), (req, res) => {
+router.get('/api/', cors(), (req, res) => {
   res.send('** Server is up and running **', {
     "Access-Control-Allow-Origin": "*"
   })
